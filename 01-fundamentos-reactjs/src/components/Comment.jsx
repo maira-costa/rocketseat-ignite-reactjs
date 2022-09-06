a1b6955139f2dbd837b38643ecdb0a452c50cae8
Maíra Costa <mairaccst@gmail.com>
2022-09-06 20:02:09 -0300

    style sidebar, add post and comments

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/84477307?v=4" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Mylena Azevedo</strong>
+                            <time 
+                                title="11 de Maio às 08:13h" 
+                                dateTime="2022-05-11 08:13:00" 
+                            >
+                                Cerca de 1h atrás
+                            </time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Maíra, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
