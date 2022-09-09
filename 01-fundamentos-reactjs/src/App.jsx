ef7695cf1a44f80f3b046b35c1a3d41d49e8cde3
Maíra Costa <mairaccst@gmail.com>
2022-09-08 22:48:00 -0300

    add posts and comments iterations

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,56 @@ import './global.css';
 
 import styles from './App.module.css';
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/81943846?v=4',
+      name: 'Maíra Costa',
+      role: 'Web Dev'
+    },
+    content: [
+      {
+        type: 'paragraph', 
+        content:'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph', 
+        content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link', 
+        content:'jane.design/doctorcare',
+      }      
+    ],
+    publishedAt: new Date('2022-09-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/84477307?v=4',
+      name: 'Mylena Azevedo',
+      role: 'Web Dev'
+    },
+    content: [
+      {
+        type: 'paragraph', 
+        content:'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph', 
+        content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link', 
+        content:'jane.design/doctorcare',
+      }      
+    ],
+    publishedAt: new Date('2022-09-08 20:00:00'),
+  },
+]
+
 export function App() {
 
   return (
@@ -14,14 +64,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Maíra Costa" 
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, velit? Repudiandae maxime illum possimus eligendi blanditiis, suscipit adipisci explicabo repellendus dolore velit. Eos rerum voluptas optio, fugit consequuntur reprehenderit maxime!" 
-          />
-          <Post 
-            author="Mylena Azevedo" 
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, velit? Repudiandae maxime illum possimus eligendi blanditiis, suscipit adipisci explicabo repellendus dolore velit. Eos rerum voluptas optio, fugit consequuntur reprehenderit maxime!" 
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
