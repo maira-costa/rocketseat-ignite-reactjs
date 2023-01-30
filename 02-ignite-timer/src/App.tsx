28424cb04d6ac182a421d40ee964c00723d3a949
Maíra Costa <mairaccst@gmail.com>
2023-01-30 20:32:21 -0300

    Updates aula Estrutura da Aplicação

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,16 +1,18 @@
-import { Button } from "./components/Button"
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
-    <>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="danger"/>
-      <Button variant="success"/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
 
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-
